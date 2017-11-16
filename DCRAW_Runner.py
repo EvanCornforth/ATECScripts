2645522fb3ed8a7dacd5b3bae35063e56bd174d7
Evan Cornforth <evancornforth@gmail.com>
2017-11-16 15:27:11 +0000

    Python file that runs the .bat file which runs dcraw, will provide the .bat file with a directory with which it is to convert all the .arw files inside it

---
+++ DCRAW_Runner.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 16 09:40:14 2017
+
+@author: Evan
+"""
+
+import os
+
+path = 'C:/Users/Evan/Documents/PhD/DCIM/testDCRAW/'
+
+os.system('C:\\Users\\Evan\\Documents\\PhD\\ATEC_Jupyter\\Convert_Dir_ARWTIFF.bat ' + path)
