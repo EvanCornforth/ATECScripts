9feb54965bab025c51feb0da71807626ab47420d
Evan Cornforth <evancornforth@gmail.com>
2017-11-16 16:24:59 +0000

    Added the ability for the .bat file to count how many images it as converted, and given the python script that runs it the ability to tell the .bat files how many images it needs to convert

--- Convert_Dir_ARWTIFF.bat
+++ Convert_Dir_ARWTIFF.bat
@@ -2,6 +2,13 @@ cd "%~dp0"
 
 @echo off
 echo Directory of .ARW files to be converted to .TIFF files is: %1
+echo Number of files to be converted is: %2
+
+REM Activate the delay expected for referencing feature
+setlocal ENABLEDELAYEDEXPANSION
+
+REM c is the counter for how many images have been converted
+set /a c=1
 
 REM %f being a variable, will loop through all the .arw images in this directory
 for %%f in (%1*.arw) do (
@@ -16,4 +23,12 @@ for %%f in (%1*.arw) do (
 			
 			REM ~n expands %f to a file name only
 			C:\Users\Evan\Desktop\dcraw-9.27-ms-64-bit\dcraw-9.27-ms-64-bit.exe -v -w -H 1 -o 1 -q 3 -4 -T %1%%~nf.arw
-    )
+			
+			REM Variables that should be delay expanded are referenced with !VARIABLE! instead of %VARIABLE%
+			echo Converted file !c! of %2
+			set /a c=c+1
+	
+	)
+	
+REM Deactivate the delay expected for referencing feature
+endlocal 
--- DCRAW_Runner.py
+++ DCRAW_Runner.py
@@ -8,6 +8,9 @@ Created on Thu Nov 16 09:40:14 2017
 import os
 
 path = 'C:/Users/Evan/Documents/PhD/DCIM/testDCRAW/'
+arwCounter = 0   
+for file in os.listdir(path):
+    if file.endswith(".arw"):
+        arwCounter = arwCounter + 1
 
-#os.system('C:\\Users\\Evan\\Documents\\PhD\\ATEC_Jupyter\\Convert_Dir_ARWTIFF.bat ' + path)
-os.system('C:\\Users\\Evan\\Documents\\PhD\\ATEC_Jupyter\\mybatch.bat ' + path)
+os.system('C:\\Users\\Evan\\Documents\\PhD\\ATEC_Jupyter\\Convert_Dir_ARWTIFF.bat ' + path + " " + str(arwCounter))
